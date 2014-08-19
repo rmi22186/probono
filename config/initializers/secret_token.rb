e7ce9034a6c44f0fa7303f72624414ef7be3a777
Robert Ing <rmi22186@gmail.com>
2014-08-19 14:03:17 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Probono::Application.config.secret_key_base = '02940c5c56cfaee55016a512e187f9a4db8f57f54871f465305cbff2a39bab80dd5a35afa0390084fa4b329bd9cf8f7de0e3b2064cfe22eff07186fe33582e0c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Probono::Application.config.session_store :cookie_store, key: '_probono_session'
