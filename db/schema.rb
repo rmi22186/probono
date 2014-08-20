469ed17e4df753af40ec256ac2782c86544e3545
Amir Kanpurwala <akanpurwala@gmail.com>
2014-08-20 16:52:47 -0700

    Adding authorization for user sign-up.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140820224904) do
+ActiveRecord::Schema.define(version: 20140820234737) do
 
   create_table "apartment_users", force: true do |t|
     t.integer  "apartment_id"
@@ -107,8 +107,10 @@ ActiveRecord::Schema.define(version: 20140820224904) do
     t.datetime "updated_at"
     t.string   "role"
     t.boolean  "active"
+    t.boolean  "approved"
   end
 
+  add_index "users", ["approved"], name: "index_users_on_approved"
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
